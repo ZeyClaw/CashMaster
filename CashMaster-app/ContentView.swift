b33872a9d83e3305ffe6e9bd7a635599777c43d7
ZeyClaw <godefroy.reynaud@gmail.com>
2024-10-22 21:56:34 +0200

    ajout de .toolbar pour la barre d'option en bas

--- CashMaster-app/ContentView.swift
+++ CashMaster-app/ContentView.swift
@@ -35,109 +35,103 @@ struct ContentView: View {
 				// Utilisation du fond par défaut des listes, qui s'adapte automatiquement au mode sombre/clair
 				Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all)
 				VStack {
-					// Ajouter une grille avec deux rectangles blancs côte à côte
-					LazyVGrid(columns: [
-						GridItem(.flexible()),  // Première colonne flexible
-						GridItem(.flexible())   // Deuxième colonne flexible
-					], spacing: 10) {  // Espacement entre les colonnes
-						NavigationLink(destination: AllTransactionsView(months: $months)) {
-							Rectangle()
-								.fill(Color(UIColor.secondarySystemGroupedBackground))  // Couleur de fond systeme (meme que liste)
-								.cornerRadius(15)  // Arrondir les bords des rectangles
-								.overlay(
-									VStack {
-										Text("Solde Total:")
-											.font(.headline)
-											.foregroundStyle(Color(UIColor { $0.userInterfaceStyle == .dark ? .white : .black }))
-										// Affichage du solde total
-										Text("\(totalSolde(), specifier: "%.2f") €")
-											.font(.title)
-											.foregroundStyle(totalSolde() >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+						// Ajouter une grille avec deux rectangles blancs côte à côte
+						LazyVGrid(columns: [
+							GridItem(.flexible()),  // Première colonne flexible
+							GridItem(.flexible())   // Deuxième colonne flexible
+						], spacing: 10) {  // Espacement entre les colonnes
+							NavigationLink(destination: AllTransactionsView(months: $months)) {
+								Rectangle()
+									.fill(Color(UIColor.secondarySystemGroupedBackground))  // Couleur de fond systeme (meme que liste)
+									.cornerRadius(15)  // Arrondir les bords des rectangles
+									.overlay(
+										VStack {
+											Text("Solde Total:")
+												.font(.headline)
+												.foregroundStyle(Color(UIColor { $0.userInterfaceStyle == .dark ? .white : .black }))
+											// Affichage du solde total
+											Text("\(totalSolde(), specifier: "%.2f") €")
+												.font(.title)
+												.foregroundStyle(totalSolde() >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+										}
+									)
+									.frame(height: 100)  // Hauteur des rectangles
+							}
+							
+							// Navigation vers la vue des transactions potentielles
+							NavigationLink(destination: PotentialTransactionsView()) {
+								Rectangle()
+									.fill(Color(UIColor.secondarySystemGroupedBackground))
+									.cornerRadius(15)
+									.overlay(
+										VStack {
+											Text("Futur")
+												.font(.headline)
+												.foregroundStyle(Color(UIColor { $0.userInterfaceStyle == .dark ? .white : .black }))
+											// Affichage du solde total
+											Text("\(totalPotentialBalance, specifier: "%.2f") €")
+												.font(.title)
+												.foregroundStyle(totalPotentialBalance >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+										}
+									)
+									.frame(height: 100)
+							}
+						}
+						.padding(.horizontal, 20)  // Réduire l'espace à gauche et à droite
+						.padding(.top, 10)  // Ajouter un peu d'espace en haut
+							
+							// list pour afficher les mois sous forme de liste
+							List {
+								ForEach(months) { month in
+									NavigationLink(destination: MonthView(month: $months[months.firstIndex(where: { $0.id == month.id })!])) {
+										// Affichage du nom du mois à gauche et du solde à droite
+										HStack {
+											Text(month.name)  // Nom du mois
+												.font(.title2)
+											Spacer()  // Espace flexible entre le nom et le solde
+											Text("\(month.solde, specifier: "%.2f") €")  // Solde du mois
+												.font(.title3)
+												.foregroundColor(month.solde >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+										}
 									}
-								)
-								.frame(height: 100)  // Hauteur des rectangles
-						}
-						
-						// Navigation vers la vue des transactions potentielles
-						NavigationLink(destination: PotentialTransactionsView()) {
-							Rectangle()
-								.fill(Color(UIColor.secondarySystemGroupedBackground))
-								.cornerRadius(15)
-								.overlay(
-									VStack {
-									Text("Futur")
-										.font(.headline)
-										.foregroundStyle(Color(UIColor { $0.userInterfaceStyle == .dark ? .white : .black }))
-									// Affichage du solde total
-									Text("\(totalPotentialBalance, specifier: "%.2f") €")
-										.font(.title)
-										.foregroundStyle(totalPotentialBalance >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+								}
+							}
+							.toolbar {
+								// Toolbar en bas de la page
+								ToolbarItemGroup(placement: .bottomBar) {
+									// Bouton d'ajout global en bas de l'écran
+									Button(action: {
+										transactionAmount = nil
+										transactionComment = ""
+										transactionDate = Date()  // Réinitialise à la date actuelle
+										transactionType = "-"
+										showingAddTransactionSheet = true
+									}) {
+										HStack {
+											Image(systemName: "plus.circle.fill")
+												.foregroundColor(.blue)
+											Text("Transaction")
+												.foregroundColor(.blue)
+												.font(.headline)
+										}
 									}
-								)
-								.frame(height: 100)
-						}
-					}
-					.padding(.horizontal, 20)  // Réduire l'espace à gauche et à droite
-					.padding(.top, 10)  // Ajouter un peu d'espace en haut
-					
-					
-					// list pour afficher les mois sous forme de liste
-					List {
-						ForEach(months) { month in
-							NavigationLink(destination: MonthView(month: $months[months.firstIndex(where: { $0.id == month.id })!])) {
-								// Affichage du nom du mois à gauche et du solde à droite
-								HStack {
-									Text(month.name)  // Nom du mois
-										.font(.title2)
-									Spacer()  // Espace flexible entre le nom et le solde
-									Text("\(month.solde, specifier: "%.2f") €")  // Solde du mois
-										.font(.title3)
-										.foregroundColor(month.solde >= 0 ? .green : .red)  // Vert si positif, rouge si négatif
+									
+									.padding()
+									
+									Spacer()// Sépare les deux boutons
+									
+									Button(action: {
+										showingResetAlert = true  // Affiche l'alerte de confirmation
+									}) {
+										HStack {
+											Image(systemName: "trash")  // Icône poubelle
+											Text("Reset")
+												.font(.headline)
+										}
+									}
+									.padding()
 								}
 							}
-						}
-					}
-					
-					
-					
-					
-					ZStack {
-						Color.clear  // Fond sans couleur
-							.blur(radius: 20)     // Application du flou SwiftUI
-							.frame(height: 60)    // Hauteur de la bande
-						HStack {
-							// Bouton d'ajout global en bas de l'écran
-							Button(action: {
-								transactionAmount = nil
-								transactionComment = ""
-								transactionDate = Date()  // Réinitialise à la date actuelle
-								transactionType = "-"
-								showingAddTransactionSheet = true
-							}) {
-								HStack {
-									Image(systemName: "plus.circle.fill")
-										.foregroundColor(.blue)
-									Text("Ajouter une Transaction")
-										.foregroundColor(.blue)
-										.font(.headline)
-								}
-							}
-							
-							.padding()
-							
-							Spacer()// Sépare les deux boutons
-							
-							Button(action: {
-								showingResetAlert = true  // Affiche l'alerte de confirmation
-							}) {
-								HStack {
-									Image(systemName: "trash")  // Icône poubelle
-									Text("Reset")
-										.font(.headline)
-								}
-							}
-							.padding()
-						}
 						.sheet(isPresented: $showingAddTransactionSheet) {
 							AddTransactionView(
 								transactionAmount: $transactionAmount,
@@ -148,7 +142,7 @@ struct ContentView: View {
 								showingAddTransactionSheet: $showingAddTransactionSheet  // Liaison pour gérer la fermeture
 							)
 						}
-					}
+					
 					.onAppear {
 						// Charger le solde total des transactions potentielles lorsque la vue apparaît
 						totalPotentialBalance = getTotalPotentialBalance()
